a4cb4e2d4d98c7ba94f8ba154790bb3fd0ba0982
mxjakubiec <maksymilian.jakubiec@microsoft.wsei.edu.pl>
2023-06-12 23:17:23 +0200

    LoginVM klasa (część)

---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace pj.ViewModel
+{
+    public  class LoginViewModel : ViewModelBase
+    {
+        //Pola
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        //Properties
+        public string Username
+        {
+            get 
+            { 
+                return _username; 
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public SecureString Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+
+        }
+
+        public string ErrorMessage
+        {
+            get
+            {
+                return _errorMessage;
+            }
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+
+        public bool IsViewVisible
+        {
+            get
+            {
+                return _isViewVisible;
+            }
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+
+        //Komendy
+        public ICommand LoginCommand { get; }
+        public ICommand RecoverPasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+        //Konstruktor
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelComand(ExexuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new ViewModelComand(p => ExecuteRecoverPassCommand("",""));
+        }
+
+        private void ExecuteRecoverPassCommand(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if (string.IsNullOrWhiteSpace(Username) || Username.Length < 3 || Password == null || Password.Length < 3)
+                validData = false;
+            else
+                validData = true;
+            return validData;
+        }
+
+        private void ExexuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
