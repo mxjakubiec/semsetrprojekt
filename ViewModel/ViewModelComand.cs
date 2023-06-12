d3abe39436c199af80047e700f1084cdfbc8084c
mxjakubiec <maksymilian.jakubiec@microsoft.wsei.edu.pl>
2023-06-12 22:36:59 +0200

    klasa VMBase i Comand

---
+++ ViewModel/ViewModelComand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace pj.ViewModel
+{
+    public class ViewModelComand : ICommand
+    {
+        //Pola 
+        private readonly Action<object> _executeAction;
+        private readonly Predicate<object> _canExecuteAction;
+
+
+        //Konstruktory
+        public ViewModelComand(Action<object> executeAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = null ;
+        }
+
+        public ViewModelComand(Action<object> executeAction, Predicate<object> canExecuteAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = canExecuteAction;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add {CommandManager.RequerySuggested += value;}
+            remove {CommandManager.RequerySuggested -= value;}
+        }
+
+        //Metody
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteAction == null ? true : _canExecuteAction(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _executeAction(parameter);
+        }
+    }
+}
