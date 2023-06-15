cd860fa8b9cdb8dbe508f6c2f7a5e3700ef3c883
mxjakubiec <maksymilian.jakubiec@microsoft.wsei.edu.pl>
2023-06-15 19:27:22 +0200

    Repositories

--- Repositories/UserRepositories.cs
+++ Repositories/UserRepositories.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using pj.Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace pj.Repositories
 {
-    internal class UserRepositories
+    public class UserRepositories : RepositoriesBase, IUserRepository
     {
+        public void Add(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AuthenticateUser(NetworkCredential credential)
+        {
+            bool validUser;
+            using (var connection = GetConnection())
+            using (var command =  new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.Connection = connection;
+                command.CommandText = "select *from uzytkownik_ where nazwaU=@username and haslo=@password ";
+                command.Parameters.Add("@username", SqlDbType.NVarChar).Value = credential.UserName;
+                command.Parameters.Add("@password", SqlDbType.NVarChar).Value = credential.Password;
+                validUser = command.ExecuteScalar() == null ? false : true;
+
+            }
+            return validUser;
+
+        }
+
+        public void Edit(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
